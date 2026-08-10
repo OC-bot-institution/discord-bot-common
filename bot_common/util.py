7a109704aff18022138ef15d156a6f0905b07580
hamu132 <guba06120@gmail.com>
2026-08-10 09:28:00 +0900

    どっちも読めるようにしたやつ

--- bot_common/util.py
+++ bot_common/util.py
@@ -3,11 +3,19 @@ from pathlib import Path
 
 
 PACKAGE_ROOT = Path(__file__).parent
-DATA_DIR = PACKAGE_ROOT / "data"
+COMMON_DATA_DIR = PACKAGE_ROOT / "data"
+
+
+def _load_json(path: Path):
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
 def load_json(filename: str):
-    path = DATA_DIR / filename
+    """Bot側のJSONを読み込む"""
+    return _load_json(Path(filename))
 
-    with open(path, "r", encoding="utf-8") as f:
-        return json.load(f)
+
+def load_common_json(filename: str):
+    """共通パッケージのJSONを読み込む"""
+    return _load_json(COMMON_DATA_DIR / filename)
