b31e15638c4dd5150f6a18295d810d3b6f595d80
hamu132 <guba06120@gmail.com>
2026-08-10 09:23:02 +0900

    jsonも共通にしてみようね

--- bot_common/util.py
+++ bot_common/util.py
@@ -1,6 +1,13 @@
-from pathlib import Path
 import json
-def load_json(filename):
-    path = Path(__file__).parent / filename
+from pathlib import Path
+
+
+PACKAGE_ROOT = Path(__file__).parent
+DATA_DIR = PACKAGE_ROOT / "data"
+
+
+def load_json(filename: str):
+    path = DATA_DIR / filename
+
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
